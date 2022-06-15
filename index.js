7450273ebbdd26bbbd93d06e25a17e20f7bb0e33
Yahya <yahyahussein670@gmail.com>
2022-06-15 14:20:34 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((response) => {
+    return response.json();
+  })
+  .then((JsonData) => {
+    return renderBooks(JsonData);
+  });
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
